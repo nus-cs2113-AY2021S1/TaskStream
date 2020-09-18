0dcc4e371c615899794d422d53affd8e703851a2
Wei Chin <eyoweichin@hotmail.com>
2020-09-18 17:01:50 +0800

    update Main.java print and count Deadlines using strings

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -24,6 +24,8 @@ public class Main {
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
+        printDeadlineUsingStreams(tasksData);
+        System.out.println("Total Deadline: " + countDeadlinesUsingStreams(tasksData));
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
